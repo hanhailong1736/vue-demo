5d9a7047a3865317b3b91e454a21518d9f9bef45
hanhailong <hlmyprivate@163.com>
2020-08-11 14:22:45 +0800

    up

--- config/index.js
+++ config/index.js
@@ -58,7 +58,7 @@ module.exports = {
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist/demo'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/demo/',
+        assetsPublicPath: './',
 
         /**
          * Source Maps
